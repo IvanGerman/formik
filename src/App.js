c71fae442b58acad63b5dd25ea7105c3e9ae37d2
IvanGerman <ivgvi@mail.ru>
2023-01-14 00:12:01 +0700

    feat: implement SignupForm2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import SignupForm1 from './components/Formik/SignupForm1';
+import SignupForm2 from './components/Formik/SignupForm2';
 
 function App() {
   return (
     <div className="App">
       <SignupForm1 />
+      <SignupForm2 />
     </div>
   );
 }
